162cc9151e7d407ece50bbc452c44be129600210
mtopalid <meropi.topalidou@inria.fr>
2015-12-15 15:07:51 +0100

    Updated for python 3 version. The 2to3 didn't do the job.

--- cython/setup.py
+++ cython/setup.py
@@ -1,7 +1,12 @@
 import numpy
-from distutils.core import setup
+from distutils.core import setup, Extension
 from Cython.Build import cythonize
 
+# setup(
+#    ext_modules = cythonize("c_dana.pyx"),
+#    include_dirs=[numpy.get_include()], requires=['numpy', 'numpy'],)
 setup(
-    ext_modules = cythonize("c_dana.pyx"),
-    include_dirs=[numpy.get_include()], requires=['numpy', 'numpy'])
+    ext_modules=[
+        Extension("c_dana", ["c_dana.c"],
+                  include_dirs=[numpy.get_include()]),],
+)
